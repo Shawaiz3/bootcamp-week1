5f93e9db67efd024047c7ad89a6ab9d030ce2e34
Shawaiz Ahmed <shawaizahmed010@gmail.com>
2025-07-25 17:16:55 +0500

    Updated some files on requirements

--- greeter.js
+++ greeter.js
@@ -1,7 +1,21 @@
-//Task 1
+// //Task 1
 
 function greet(name) {
     return `Hello, ${name}`
 }
-export {greet};
-export default greet;
+
+const customArgs = process.argv.slice(2);
+
+customArgs.forEach(el=>{
+    console.log(greet(el))
+})
+
+// [
+//     'shahzeb',
+//     'sharif',
+//     'shahwaiz',
+// ].forEach(el=>{
+//     console.log(greet(el))
+// })
+
+
